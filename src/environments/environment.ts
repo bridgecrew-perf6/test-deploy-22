67b96f8cfcfbbbdb010c7874e6399538b84735fa
quocdatdev <quocdatsh93@gmail.com>
2022-05-21 20:59:19 +0700

    Revert enviroment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyA3bE-r1jB9IIUAaCsKEkAPF2mB-PQP5fk',
-    authDomain: 'storedatacrud.firebaseapp.com',
-    projectId: 'storedatacrud',
-    storageBucket: 'storedatacrud.appspot.com',
-    messagingSenderId: '929824991359',
-    appId: '1:929824991359:web:46843481ffc0c18bee374a',
-    measurementId: 'G-7RN1C39M15',
-  },
+  // firebase: {
+  //   apiKey: 'AIzaSyA3bE-r1jB9IIUAaCsKEkAPF2mB-PQP5fk',
+  //   authDomain: 'storedatacrud.firebaseapp.com',
+  //   projectId: 'storedatacrud',
+  //   storageBucket: 'storedatacrud.appspot.com',
+  //   messagingSenderId: '929824991359',
+  //   appId: '1:929824991359:web:46843481ffc0c18bee374a',
+  //   measurementId: 'G-7RN1C39M15',
+  // },
 };
